95662955ad5664d27ce669431590b7520580eb8b
Kianoosh Soleimani <kianooshsoleimani@gmail.com>
2021-02-01 15:13:43 +0330

    fix something

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,13 +21,7 @@ class Footer extends Component {
             <ul className='social-links'>{networks}</ul>
 
             <ul className='copyright'>
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>
-                Design by{' '}
-                <a title='Styleshout' href='http://www.styleshout.com/'>
-                  Styleshout
-                </a>
-              </li>
+              <li>&copy; Copyright 2020 Kianoosh Soleimani</li>
             </ul>
           </div>
           <div id='go-top'>
