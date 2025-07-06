29905118cfdb1caa7775d309fc3def5d15fa5ec5
Kianoosh Soleimani <kianooshsoleimani@gmail.com>
2025-07-06 22:53:10 +0330

    Replace index.html with React portfolio app

---
+++ precache-manifest.a805e2d2ec349d7bf1f98300d0d0fabb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf39def8443c00690321",
+    "url": "/static/css/main.52eba9e7.chunk.css"
+  },
+  {
+    "revision": "cf39def8443c00690321",
+    "url": "/static/js/main.cf39def8.chunk.js"
+  },
+  {
+    "revision": "67f4f9dea4790a10ad38",
+    "url": "/static/js/1.67f4f9de.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dfe6a7347099daab69300ded367d1b44",
+    "url": "/index.html"
+  }
+];
